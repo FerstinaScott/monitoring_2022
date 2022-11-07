668a0153d303049093f7a6be015a78c227430fae
Ferstina Wady Scott <111440067+FerstinaScott@users.noreply.github.com>
2022-11-07 03:10:57 +0100

    Create R_code_ecol_relationships.r

---
+++ R_code_ecol_relationships.r
@@ -0,0 +1,18 @@
+# Here you can write everything! This is a comment!
+
+# R code for plotting the relationship among ecological variables
+
+# install.packages is used to install packages
+install.packages("sp")
+library(sp) 
+
+# data is used to recall datasets
+data(meuse)
+
+# llok inside the set!
+meuse
+
+View(meuse) # it might not work for mac
+# solve using: https://www.xquartz.org
+
+head(meuse)
